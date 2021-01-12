484fea0763f907da4dc3aac7aa9dd6e0c9ef0933
ruot-nyak <nyak.g.ruot@gmail.com>
2021-01-12 14:40:28 -0800

    Abillity to check current stats on specific stocks.

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ from bs4 import BeautifulSoup as bs
 import html5lib
 
 
+stock = input('What is your stock tag? ')
+
 #requesting the url for desired page to scrape
-r = requests.get("https://www.marketwatch.com/investing/stock/gm/analystestimates?mod=mw_quote_tab")
+r = requests.get(f"https://www.marketwatch.com/investing/stock/{stock}/analystestimates?mod=mw_quote_tab")
 
 #grabbing all of the information on the page
 page = bs(r.content, features="html5lib")
@@ -38,31 +40,31 @@ class bot:
         high_num = str(numbers[0])
         #pulling the float from the parsed information, without this returns strange string
         high = rx.findall(high_num)  
-        print(f'The High for GM is, ${high[1]}')
+        print(f'The High for {stock} is, ${high[1]}')
         return high
 
     def return_low(self,low):
         low_num = str(numbers[2])
         low = rx.findall(low_num)
-        print(f'The low for GM is, ${low[1]}')
+        print(f'The low for {stock} is, ${low[1]}')
         return low
 
     def return_median(self,median):
         median_num = str(numbers[1])
         median = rx.findall(median_num)
-        print(f'The median for GM is, ${median[1]}')
+        print(f'The median for {stock} is, ${median[1]}')
         return median
 
     def return_average(self,average):
         average_num = str(numbers[3])
         self.average = rx.findall(average_num)
-        print(f'The average for GM is, ${self.average[1]}')
+        print(f'The average for {stock} is, ${self.average[1]}')
         return self.average[1]
 
     def return_current_price(self,current_price):
         current_price_num = str(numbers[4])
         self.current_price = rx.findall(current_price_num)
-        print(f'The current value of GM is, ${self.current_price[1]}')
+        print(f'The current value of {stock} is, ${self.current_price[1]}')
         return self.current_price[1]
 
     # def choice(self,):
@@ -75,9 +77,12 @@ class bot:
 
 
 
-print(tables[1])
-print(numbers)
-test = bot('Ruot',0,0,0,0)
+# print(tables[1])
+# print(numbers)
+test = bot('',0,0,0,0)
 test.return_high('')
+test.return_low('')
+test.return_median('')
+test.return_average('')
 test.return_current_price('')
 # test.choice()
